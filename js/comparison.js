fe1b11b9d9ad00e33db4b74ad874cf19dd535127
MOHAMMAD DAYLAKI <mohammad.daylaky@gmail.com>
2017-01-06 01:07:42 +0000

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,25 @@
 'use strict';
+function testFalseLooseEqualityComparison() {
+  var zero= 0, one= 1;
+return (zero ==one)
+}
+
+function testTrueLooseNonEqualityComparison() {
+var zero=0 ,one=1;
+return (zero!=one)
+}
+
+function testTrueLooseEqualityComparison() {
+var num=1 ,str="1";
+return (num==str)
+}
+
+function testFalseStrictEqualityComparison() {
+  var num=1 ,str="1";
+  return (num===str)
+}
+
+function testTrueStrictEqualityComparison() {
+  var num=1 ,str="1";
+  return (num===parseInt(str))
+}
