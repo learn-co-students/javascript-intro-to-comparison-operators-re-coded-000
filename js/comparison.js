0d752fdfcd820a0e8b8242a68044c482c94bc009
fatmaMY <fatima.mohammad736@gmail.com>
2017-02-16 21:12:12 +0300

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,30 @@
 'use strict';
+function testFalseLooseEqualityComparison() {
+	var zero = 0;
+	var one = 1;
+	return zero == one;
+}
+
+function testTrueLooseNonEqualityComparison() {
+	var zero = 0;
+	var one = 1;
+	return zero != one;
+}
+
+function testTrueLooseEqualityComparison() {
+	var one = 1;
+	var oneString = "1";
+	return one == oneString;
+}
+
+function testFalseStrictEqualityComparison(argument) {
+	var one = 1;
+	var oneString = "1";
+	return one === oneString;
+}
+
+function testTrueStrictEqualityComparison() {
+	var one = 1;
+	var oneString = "1";
+	return one === parseInt(oneString);
+}
