60d3c2b0eb3a0847aea4393dd938102da9eab151
Mustafa Taha <mtmax1994@gmail.com>
2017-02-20 02:29:49 +0300

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,26 @@
 'use strict';
+
+var first=0;
+var second=1;
+
+function testFalseLooseEqualityComparison(){
+	return first == second;
+}
+
+function testTrueLooseNonEqualityComparison(){
+	return first != second;
+}
+
+var first_value=1;
+var second_value="1";
+function testTrueLooseEqualityComparison(){
+	return first_value == second_value;
+}
+
+function testFalseStrictEqualityComparison(){
+	return first_value === second_value;
+}
+
+function testTrueStrictEqualityComparison(){
+	return first_value === parseInt(second_value);
+}
