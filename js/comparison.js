6ab59cf7627200668332f1a080d5b84f9387ecbb
abdalkalk <eng.abdalkalk@gmail.com>
2017-02-20 13:38:57 +0300

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,27 @@
 'use strict';
+function testFalseLooseEqualityComparison(){
+  var x=0;
+  var y=1;
+  return x==y;
+}
+function testTrueLooseNonEqualityComparison() {
+  var x=0;
+  var y=1;
+  return x!==y;
+}
+function testTrueLooseEqualityComparison () {
+  var x="1";
+  var y=1;
+  return x==y;
+}
+function testFalseStrictEqualityComparison () {
+  var x="1";
+  var y=1;
+  return x===y;
+}
+function testTrueStrictEqualityComparison () {
+  var x="1";x=parseInt(x);
+  var y=1;
+  return x===y;
+}
+
