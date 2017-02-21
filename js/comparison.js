6de7a4bd6ba80e28604af07cce4dabb8faa6818b
Ali Diyab <alidiyab1997@gmail.com>
2017-02-21 22:44:15 +0000

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,26 @@
 'use strict';
+function testFalseLooseEqualityComparison(){
+  var x = 0;
+  var z = 1;
+  return (x == z)
+};
+function testTrueLooseNonEqualityComparison(){
+  var x = 0;
+  var z = 1;
+  return (x != z)
+}
+function testTrueLooseEqualityComparison(){
+  var x = 1;
+  var z = "1";
+  return (x == z)
+}
+function testFalseStrictEqualityComparison(){
+  var x = 1;
+  var z = "1";
+  return (x === z)
+}
+function testTrueStrictEqualityComparison(){
+  var x = 1;
+  var z = "1";
+  return (x == z)
+}
