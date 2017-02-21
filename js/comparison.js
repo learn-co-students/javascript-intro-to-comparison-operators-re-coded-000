516dcb731667a8d75f6e4d2eeec3582f63d1d1ff
Nichervan <nichervan.brhik@gmail.com>
2017-02-21 08:35:29 +0300

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+	var x = 0;
+	var y = 1;
+		return x==y;
+	// body...
+}
+
+
+function testTrueLooseNonEqualityComparison() {
+	var x = 0;
+	var y = 1;
+     return  x!=y;
+
+}
+
+function testTrueLooseEqualityComparison(){
+	var x = 1;
+	var y = "1"
+	return 1=="1"
+}
+
+
+function testFalseStrictEqualityComparison() {
+	var x = 0;
+	var y = 1;
+	         
+	       return x===y;
+	
+}
+
+function testTrueStrictEqualityComparison() {
+	var x = 1;
+	var y = "1";
+	         
+	       return x=== parseInt (y)
+}
