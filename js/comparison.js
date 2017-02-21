113e21d59fc36e99135c1b7d801fe025ff9eaf3a
Saber Rasheed <edrees.rasheed@yahoo.com>
2017-02-21 23:42:53 +0300

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function testFalseLooseEqualityComparison(){
+  var d = 0;
+  var f = 1;  
+  return d == f;
+}
+
+function testTrueLooseNonEqualityComparison(){
+  var d = 0;
+  var f = 1;  
+  return d != f;
+}
+
+function testTrueLooseEqualityComparison(){
+  var f = 1;  
+  var d = "1"
+  return f == d;
+}
+
+
+function testFalseStrictEqualityComparison(){
+  var f = 1;
+  var d = '1';
+  return f === d;
+}
+
+function testTrueStrictEqualityComparison(){
+  var f = 1;
+  var d = '1';
+  return f === parseInt(d);
+}
