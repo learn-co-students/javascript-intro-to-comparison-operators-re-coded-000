ab7f6b58df5b1f7a06e029401c996bdd955ec481
Ekhlas Rain <baran.rain1994@gmail.com>
2017-02-21 11:26:35 +0300

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,35 @@
 'use strict';
+function testFalseLooseEqualityComparison(){
+	var x= 0;
+    var y= 1;
+    return 0 == 1;
+    return false;
+}
+
+function testTrueLooseNonEqualityComparison(){
+	var x= 0;
+	var y= 1;
+	return 0 != 1;
+	return true;
+}
+
+function testTrueLooseEqualityComparison(){
+	var x= 1;
+	var y= "1";
+	return x == y;
+	return true
+}
+
+function testFalseStrictEqualityComparison(){
+	var x= 1;
+	var y= "1";
+	return x === y;
+	return false
+}
+
+function testTrueStrictEqualityComparison(){
+	var x= 1;
+	var y= "1";
+	return 1 === parseInt("1");
+}
+
