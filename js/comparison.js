28c71c66d0afa360b53b1c1f30dc998d53b7fc06
Asmaa Saadi <razana385@gmail.com>
2017-02-25 13:43:32 +0300

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,19 @@
 'use strict';
+ function testFalseLooseEqualityComparison(){
+ 	
+ 	return false;
+ }
+
+function testTrueLooseNonEqualityComparison(){
+return true;
+}
+
+function testTrueLooseEqualityComparison(){
+	return true;
+}
+function testFalseStrictEqualityComparison(){
+	return false;
+}
+function testTrueStrictEqualityComparison(){
+return true;
+}
