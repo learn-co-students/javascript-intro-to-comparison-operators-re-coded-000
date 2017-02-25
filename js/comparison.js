b382382f65bf560b8236949ed18160a8d3f57fbe
dina aziz <dina.aziz100@yahoo.com>
2017-02-25 22:19:07 +0000

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function testFalseLooseEqualityComparison()
+{ var integer1 = 0;
+  var integer2 = 1;
+  return integer1 == integer2;}
+
+function testTrueLooseNonEqualityComparison()
+{ var integer1 = 0;
+  var integer2 = 1;
+  return  integer1 != integer2;}
+
+function testTrueLooseEqualityComparison()
+{ var integer1 = 1;
+  var integer2 = "1"
+  return integer1 == integer2;}
+
+
+function testFalseStrictEqualityComparison()
+{ var integer1 = 1;
+  var integer2= '1';
+  return integer1 === integer2;}
+
+function testTrueStrictEqualityComparison()
+{ var integer1 = 1;
+  var integer2 = '1';
+  return integer1 === parseInt(integer2);}
